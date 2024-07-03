1d338b2c2cce287caa24a2421fe19054d0abfd17
ankitchaahat <chaahat17sep@gmail.com>
2024-07-03 18:05:22 +0530

    Update OLS_2.py
    Used scikit learn to find the value of parameters and compared with that mathematical formula .

--- OLS_2.py
+++ OLS_2.py
@@ -1,1 +1,16 @@
+from sklearn.linear_model import LinearRegression
 
+LR=LinearRegression()
+
+# define your X and Y as NumPy Arrays (column vectors)
+X = np.array([1,3,5]).reshape(-1,1)
+Y = np.array([4.8,12.4,15.5]).reshape(-1,1)
+
+LR.fit(X,Y) # calculate the model coefficients
+LR.intercept_ # the bias or the intercept term (w0*)
+
+#OUTPUT -->  array([2.875])    COMPARE IT WITH THE FILE ols.py
+
+LR.coef_ # the slope term (w1*)
+
+# OUTPUT --> array([[2.675]]) COMPARE IT WITH THE FILE ols.py
